bab9299df8714a04139f2ec74b8702749e65f7df
Tiago Fernandes <tiagoferprojects@gmail.com>
2024-01-07 14:32:54 -0300

    Appending Colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,5 +4,22 @@ export const theme = extendTheme({
   fonts: {
     heading: "Poppins",
     body: "Poppins",
+  },
+  colors: {
+    gray: {
+      "50": "#F5F8FA",
+      "100": "#DADADA",
+      "700": "#999999",
+      "800": "#47585B",
+    },
+    yellow: {
+      "500": "#FFBA08",
+    }
+  },
+  styles: {
+    global: {
+      background: "gray.50",
+      color: "gray.800"
+    }
   }
 } as Theme | {})
